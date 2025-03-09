faa296cc85aadc8d382fb0655fc658063ac40ca4
naamaheller <naama.he788@gmail.com>
2025-03-10 00:33:24 +0200

    hazpana

--- models/order.js
+++ models/order.js
@@ -1,12 +1,13 @@
-import { Schema, Types, model } from "mongoose";
-import { productSchema } from "./product.js";
+import { Schema, Types, model } from "mongoose"
+import { productSchema } from "./product.js"
 
 const minimalSchema = Schema({
-    product:productSchema,
-    count:{type:Number,default:0}
+    product: productSchema,
+    count: { type: Number, default: 0 }
 })
 
 const orderSchema = Schema({
+    orderNumber: { type: Number, unique: true, default: () => Math.floor(100000 + Math.random() * 900000) },
     date: { type: Date, default: new Date() },
     dateEnd: { type: Date, default: new Date() + 1 },
     address: String,
@@ -14,10 +15,10 @@ const orderSchema = Schema({
         type: Types.ObjectId,
         ref: "users"
     },
-    products:[minimalSchema],
-    isSend:{type:Boolean,default:false},
+    products: [minimalSchema],
+    isSend: { type: Boolean, default: false },
     price: Number,
-    finalPrice:Number
+    finalPrice: Number,
 })
 
-export const orderModel = model("order", orderSchema);
+export const orderModel = model("order", orderSchema)
